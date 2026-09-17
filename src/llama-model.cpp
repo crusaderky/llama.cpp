6dc2680b43240e25feda948590cb0ac6f908fc97
crusaderky <gimperiale@openteams.com>
2026-09-17 20:57:18 +0100

    Squashed commit of model/K2Horizon

--- src/llama-model.cpp
+++ src/llama-model.cpp
@@ -340,6 +340,8 @@ static llama_model * llama_model_mapping(llm_arch arch, const llama_model_params
             return new llama_model_kimi_k3(params);
         case LLM_ARCH_STEP35:
             return new llama_model_step35(params);
+        case LLM_ARCH_K2_HORIZON:
+            return new llama_model_k2_horizon(params);
         case LLM_ARCH_SPARK2_5:
             return new llama_model_spark2_5(params);
         default:
@@ -3157,6 +3159,7 @@ llama_rope_type llama_model_rope_type(const llama_model * model) {
         case LLM_ARCH_STEP35:
         case LLM_ARCH_SPARK2_5:
         case LLM_ARCH_TALKIE:
+        case LLM_ARCH_K2_HORIZON:
         case LLM_ARCH_MELLUM:
             return LLAMA_ROPE_TYPE_NEOX;
 
